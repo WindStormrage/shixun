782816f760ec16011b864c499013ee2fb6bd247b
xhy <695729033@qq.com>
2017-12-10 15:36:32 +0800

    new2

--- src/demo1/main.java
+++ src/demo1/main.java
@@ -5,7 +5,6 @@ package demo1;
  */
 public class main {
     public static void main(String args[]){
-        System.out.print("777");
-        System.out.print("777");
+        System.out.print("888");
     }
 }
